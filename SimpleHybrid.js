1672327acb97b3b237a4b029f80d38ffebac1c86
Jung JaeYeol <jjw22new@naver.com>
2019-03-12 17:59:56 +0900

    add device check

--- SimpleHybrid.js
+++ SimpleHybrid.js
@@ -20,10 +20,51 @@ var SH = {
         }
     },
     
+    /*
     Run : function(viewName) {
         location.href = "SH://run?viewName=" + viewName;
     },
     Run : function(viewName, param) {
         location.href = "SH://run?viewName=" + viewName + "&param=" + param;
+    }
+    */
+
+    DeviceType : function() {
+        var agent = navigator.userAgent.toLowerCase();
+	
+        var type = "PCWeb";
+        var mobileWebTxts = [
+                "android","phone",
+                "iphone","ipad","ipod",
+                "blackberry",
+                "opera mini","opera mobi",
+                "windows ce","iemobile",
+                "nokia", "webos",
+                "sonyericsson",
+                ];
+        
+        if(agent.indexOf('SimpleHybrid') != -1){
+            if(agent.indexOf('android') != -1) {
+                type = "AndroidApp";
+            } else if((agent.indexOf('iphone') != -1) || (agent.indexOf('ipad') != -1) || (agent.indexOf('ipod') != -1)) {
+                type = "iOSApp";
+            }
+        } else {
+            if(agent.indexOf('android') != -1) {
+                return "AndroidWeb";
+            } else if((agent.indexOf('iphone') != -1) || (agent.indexOf('ipad') != -1) || (agent.indexOf('ipod') != -1)) {
+                return "iOSWeb";
+            } else {
+                for (var i = 0; i < mobileWebTxts.length; i++){
+                    var result = agent.indexOf(mobileWebTxts[i]);
+                    if(result != -1){
+                        type ='MobileWeb';
+                        break;
+                    }
+                }
+            }
+        }
+        
+        return type;
     }
 }
